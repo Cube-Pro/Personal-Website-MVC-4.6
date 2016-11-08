85d9951596fdc1208c75055c0ab3b55c7f28f0b1
CubePro <cubeprofessional@gmail.com>
2016-11-08 09:23:29 -0600

    Add project files.

---
+++ Personal-Website-MVC-4.6/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Personal_Website_MVC_4._6
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Personal-Website-MVC-4.6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Personal_Website_MVC_4._6.MvcApplication" Language="C#" %>
